93e952c7deb0430137fdb1c8a8102868c94e713f
agusgarone <agusgarone02@gmail.com>
2021-10-04 15:50:49 -0300

    FIX: mediaquerys turnos and pacientes

--- server/src/service/mail/module/mail.service.ts
+++ server/src/service/mail/module/mail.service.ts
@@ -1,42 +1,40 @@
-"use strict";
-import { Body, Injectable, Logger } from "@nestjs/common";
-import { MedicoDTO } from "src/service/dto/medico.dto";
-import { PacienteDTO } from "src/service/dto/paciente.dto";
-import { TurnoDTO } from "src/service/dto/turno.dto";
+'use strict';
+import { Body, Injectable, Logger } from '@nestjs/common';
+import { MedicoDTO } from 'src/service/dto/medico.dto';
+import { PacienteDTO } from 'src/service/dto/paciente.dto';
+import { TurnoDTO } from 'src/service/dto/turno.dto';
 
+export class mailService {
+    logger = new Logger('MailService');
+    async mailTurno(paciente: PacienteDTO, Medico: MedicoDTO, turno: TurnoDTO): Promise<PacienteDTO | undefined> {
+        const nodemailer = require('nodemailer');
+        const directTransport = require('nodemailer-direct-transport');
+        const transporter = nodemailer.createTransport(directTransport());
+        let notificacionTurno;
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: process.env.MAIL, // sender address
+            to: paciente.mail, // list of receivers
+            subject: 'Turno Asignado', // Subject line
+            text: '', // plain text body
+            html: notificacionTurno, // html body
+        });
+        return null;
+    }
 
-export class mailService{
-  logger = new Logger('MailService');
-  async mailTurno(paciente: PacienteDTO, Medico: MedicoDTO,turno: TurnoDTO): Promise<PacienteDTO | undefined>  {
-    const nodemailer = require("nodemailer");
-    var directTransport = require('nodemailer-direct-transport');
-    var transporter = nodemailer.createTransport(directTransport())
-    var notificacionTurno
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: process.env.MAIL, // sender address
-    to: paciente.mail, // list of receivers
-    subject: "Turno Asignado", // Subject line
-    text: "", // plain text body
-    html: notificacionTurno, // html body
-  });
-return null;
-}
-
-  async mailContactanos(paciente: PacienteDTO, Medico: MedicoDTO,turno: TurnoDTO): Promise<PacienteDTO | undefined>  {
-    const nodemailer = require("nodemailer");
-    var directTransport = require('nodemailer-direct-transport');
-    var transporter = nodemailer.createTransport(directTransport())
-    var notificacionTurno
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: paciente.mail, // sender address
-    to: process.env.MAIL, // list of receivers
-    subject: "mensaje de usuario", // Subject line
-    text: "", // plain text body
-    html: notificacionTurno, // html body
-  });
-return null;
-}
-
+    async mailContactanos(paciente: PacienteDTO, Medico: MedicoDTO, turno: TurnoDTO): Promise<PacienteDTO | undefined> {
+        const nodemailer = require('nodemailer');
+        const directTransport = require('nodemailer-direct-transport');
+        const transporter = nodemailer.createTransport(directTransport());
+        let notificacionTurno;
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: paciente.mail, // sender address
+            to: process.env.MAIL, // list of receivers
+            subject: 'mensaje de usuario', // Subject line
+            text: '', // plain text body
+            html: notificacionTurno, // html body
+        });
+        return null;
+    }
 }
