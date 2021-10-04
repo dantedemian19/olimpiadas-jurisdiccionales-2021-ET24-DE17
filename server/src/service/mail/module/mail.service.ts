593053c372a1f7700ae9dec1821ca50be608e686
dantedemian19 <dantedemian19@gmail.com>
2021-10-04 14:39:57 -0300

    fix mail

--- server/src/service/mail/module/mail.service.ts
+++ server/src/service/mail/module/mail.service.ts
@@ -7,7 +7,7 @@ import { TurnoDTO } from "src/service/dto/turno.dto";
 
 export class mailService{
   logger = new Logger('MailService');
-  async main(paciente: PacienteDTO, Medico: MedicoDTO,turno: TurnoDTO): Promise<PacienteDTO | undefined>  {
+  async mailTurno(paciente: PacienteDTO, Medico: MedicoDTO,turno: TurnoDTO): Promise<PacienteDTO | undefined>  {
     const nodemailer = require("nodemailer");
     var directTransport = require('nodemailer-direct-transport');
     var transporter = nodemailer.createTransport(directTransport())
@@ -17,6 +17,22 @@ export class mailService{
     from: process.env.MAIL, // sender address
     to: paciente.mail, // list of receivers
     subject: "Turno Asignado", // Subject line
+    text: "", // plain text body
+    html: notificacionTurno, // html body
+  });
+return null;
+}
+
+  async mailContactanos(paciente: PacienteDTO, Medico: MedicoDTO,turno: TurnoDTO): Promise<PacienteDTO | undefined>  {
+    const nodemailer = require("nodemailer");
+    var directTransport = require('nodemailer-direct-transport');
+    var transporter = nodemailer.createTransport(directTransport())
+    var notificacionTurno
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: paciente.mail, // sender address
+    to: process.env.MAIL, // list of receivers
+    subject: "mensaje de usuario", // Subject line
     text: "", // plain text body
     html: notificacionTurno, // html body
   });
